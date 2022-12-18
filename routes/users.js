5c98fcb6acc5aa9759ec2e440aa01f7373cf1185
agustin mercado <agustinmercado1234@gmail.com>
2022-12-18 18:47:29 -0300

    iniciado el userController y la funcion getAllUser para traer todos los usuarios, ademas del userModel para el modelo de usuario

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {getAllUsers} = require('../controllers/userController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/users', getAllUsers);
 
 module.exports = router;
