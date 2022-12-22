fe4cfb2cc7b745cf27fffb6d7ae9a225ffa45a12
agustin mercado <agustinmercado1234@gmail.com>
2022-12-21 22:43:39 -0300

    creadas las funciones para traer un solo usuario, eliminar y editar usuarios

--- models/UserModel.js
+++ models/UserModel.js
@@ -6,7 +6,11 @@ const userSchema = new Schema({
     name: {type: String, requird: true, unique: true},
     email: {type: String, requird: true},
     password: {type: String, requird: true},
+    address: String,
+    province: String,
     role: {type: String, enum:roles, default: roles.CLIENT},
+    deleteAt: Date,
+    isDeleted: Boolean,
     isActive: Boolean,
 },
 {
