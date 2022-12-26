4d1dc8cc9ce7e86dbcbe18c10bd9b32f2628ea79
agustin mercado <agustinmercado1234@gmail.com>
2022-12-26 17:08:22 -0300

    creacion de middleware para confirmar si el usuario esta logueado y otro para verificar el rol del usuario antes de poder realizar las peticiones.

---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+const User = require("../models/UserModel");
+const { getToken, verifiedToken } = require("../utils/jwtService")
+
+
+// VERIFICA QUE EL USUARIO ESTE LOGUEADO Y EL TOKEN AUN VIGENTE
+const isLoggedIn = async (req, res, next)=>{
+    const token = getToken(req);
+    if(!token){
+        return res.status(401).send('Usuario no autorizado')}
+    try {
+        const tokenDecode = verifiedToken(token);
+        const userFound = await User.findById(tokenDecode.id);
+        if(!userFound){
+            res.status(401).send('El usuario no existe o no esta autorizado');
+        } else { next(); }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Internal Server Error')
+    }
+}
+
+
+// VERIFICA QUE EL USUARIO ESTE LOGUEADO,  EL TOKEN AUN VIGENTE Y ADEMAS QUE EL ROLE DEL USUARIO SEA ADMIN 
+const checkRole = (role)=>{
+    try {
+        return async (req, res, next)=>{
+            const token = getToken(req);
+            if(!token) {return res.status(401).send('Usuario no autorizado')}
+            const tokenDecode = verifiedToken(token);
+            const userFound = await User.findById(tokenDecode.id);
+            if(userFound && userFound.role == role){
+                next();
+            } else {  res.status(401).send('Usuario no autorizado'); }
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error')
+    }
+}
+
+module.exports = {isLoggedIn, checkRole}
