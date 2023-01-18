7f339788e85736576d34181c796b02458d73619f
BeeSluka <110938398+BeeSluka@users.noreply.github.com>
2023-01-18 01:54:04 +0000

    Cambios en app.js

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const dbConnect = require("./config/db");
-const userRouter = require("./routes/users");
-const paymentRouter = require("./routes/payments");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dbConnect = require('./config/db');
+const userRouter = require('./routes/users')
+const paymentRouter = require('./routes/payments')
+const productsRouter = require("./routes/products");
+const cors = require('cors')
+const dotenv = require('dotenv');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser')
 dotenv.config();
 dbConnect();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(logger("dev"));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(logger('dev'));
 app.use(express.json());
-app.use((req, res, next) => {
-  /* en ORIGIN_URL se debe crear un archivo .env y colocar la url desde la que se haran las peticiones */
-  res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-app.use(cors({ origin: process.env.ORIGIN_URL }));
+app.use((req, res, next)=>{
+    /* en ORIGIN_URL se debe crear un archivo .env y colocar la url desde la que se haran las peticiones */
+    res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN_URL);
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
+app.use(cors({origin: process.env.ORIGIN_URL}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/api", userRouter);
-app.use("/api", paymentRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api',userRouter)
+app.use('/api',paymentRouter)
+app.use("/products", productsRouter);
+
+
+
 
 module.exports = app;
