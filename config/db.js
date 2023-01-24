aead22435a6755db784ef14aa476a147c02c4477
agustin <agustinmercado123@gmail.com>
2023-01-24 20:04:11 -0300

    config vercel

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const connectDb = () => {
+const connectDb = async () => {
   /*en MONGO_CONNECTION_STRING se debe crear un archivo .env y cologar el mongo uris para conectarse*/
   try {
-    mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
   } catch (error) {
     console.log(error);
   }
