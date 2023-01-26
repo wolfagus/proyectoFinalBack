a2375f171e16e5f36e1c444e9ff82fcf38e17608
agustin <agustinmercado123@gmail.com>
2023-01-26 12:00:18 -0300

    cambios en las cors

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Max-Age', '86400');
     next();
 })
 app.use(express.urlencoded({ extended: false }));
